657c04b755a20d30772c55b1a2024518f9b73ca3
miguelruizc <buumchakalaka@gmail.com>
2024-06-21 16:57:54 +0200

    Add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import Shop from './Shop.jsx';
+import Cart from './Cart.jsx';
+
 function App() {
-	return <h1>Awesome Fake-Shop Application</h1>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/shop" element={<Shop />}></Route>
+				<Route path="/cart" element={<Cart />}></Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
