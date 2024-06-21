e1c2130bf81123caf2b8187f6ce79e408b3c817b
miguelruizc <buumchakalaka@gmail.com>
2024-06-21 17:05:22 +0200

    Add navigation component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import Shop from './Shop.jsx';
 import Cart from './Cart.jsx';
+import Nav from './Nav.jsx';
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />}></Route>
-				<Route path="/shop" element={<Shop />}></Route>
-				<Route path="/cart" element={<Cart />}></Route>
-			</Routes>
-		</Router>
+		<div>
+			<Router>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/shop" element={<Shop />}></Route>
+					<Route path="/cart" element={<Cart />}></Route>
+				</Routes>
+			</Router>
+		</div>
 	);
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+					<Link to="/shop">Shop</Link>
+					<Link to="/cart">Cart</Link>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Nav;
