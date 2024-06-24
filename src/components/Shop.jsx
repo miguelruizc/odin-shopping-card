aa09a7032ef922597489205f9e604e3fd0cf2114
miguelruizc <ruizconde.m@gmail.com>
2024-06-24 22:59:16 +0200

    Add fetch logic to Shop component on mount

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 const Shop = () => {
+
+	useEffect(()=>{
+		fetch('https://fakestoreapi.com/products')
+		.then((res) => res.json())
+		.then((json) => console.log(json))
+		.catch(err => console.log(err));
+	}, []);
+
 	return (
 		<div className="content">
 			<h1>Shop</h1>
