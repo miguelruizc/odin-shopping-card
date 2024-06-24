f11fcc235d007f72f1694c0adb9b5130c2968153
miguelruizc <ruizconde.m@gmail.com>
2024-06-24 23:23:23 +0200

    Add shop items to shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,17 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 
 const Shop = () => {
+	const [items, setItems] = useState([]);
 
-	useEffect(()=>{
+	useEffect(() => {
 		fetch('https://fakestoreapi.com/products')
-		.then((res) => res.json())
-		.then((json) => console.log(json))
-		.catch(err => console.log(err));
+			.then((res) => {
+				if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+				return res.json();
+			})
+			.then((json) => {
+				console.log(json);
+				setItems(json);
+			})
+			.catch((err) => console.log(err));
 	}, []);
+
+	const itemsList = items.map((element) => {
+		console.log(element);
+
+		return (
+			<div className="shopItem" key={element.id}>
+				<h2>{element.title}</h2>
+				<h3>{element.price}$</h3>
+				<p>Category: {element.category}</p>
+				<p>{element.description}</p>
+				<img src={element.image} alt="item picture" />
+				<p>
+					Rating: {element.rating.rate} Reviews: {element.rating.count}
+				</p>
+
+				<form action="shopItemForm">
+					<input type="number" min="1" placeholder={1} />
+					<button type="submit">Add to cart</button>
+				</form>
+			</div>
+		);
+	});
 
 	return (
 		<div className="content">
 			<h1>Shop</h1>
+			{itemsList}
 		</div>
 	);
 };
