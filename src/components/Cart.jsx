ae35fbd8a46f148deed5d6bc213ae9e254ae6fae
miguelruizc <ruizconde.m@gmail.com>
2024-06-25 09:32:30 +0200

    Add functionaly to change quantity of items in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,43 @@
 const Cart = ({ cart, setCart }) => {
+	const handleQuantityChange = (event, id) => {
+		console.log('quantity input onChange trigger');
+		const newQuant = event.target.value;
+
+		console.log(`New quant: ${newQuant}`);
+
+		setCart((state) => {
+			let index = state.findIndex((obj) => obj.id === id);
+			let newObject = state[index];
+			let newState = [...state];
+
+			if (newQuant <= 0) {
+				// If quant <= 0: Remove cart element
+				newState.splice(index, 1);
+			} else {
+				// If quant > 0: modify cart element
+				newObject.quantity = newQuant;
+				newState[index] = newObject;
+			}
+
+			console.log(newState);
+			return newState;
+		});
+	};
+
 	const cartList = cart.map((element) => {
 		return (
 			<div key={element.id} className="cartItem">
 				<h2>{element.title}</h2>
 				<p>{element.price}</p>
 				<img src={element.image} alt="Image of item" />
-				<p>Cuantity: {element.quantity}</p>
+				<p>
+					Quantity:
+					<input
+						type="number"
+						value={element.quantity}
+						onChange={(event) => handleQuantityChange(event, element.id)}
+					/>
+				</p>
 				<p>Total: {element.price * element.quantity}</p>
 				<button>remove</button>
 				<button>buy</button>
